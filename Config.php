ef3098e087c89097526c0fa266a6f2cf86885bda
Domenico Giambra <zorinik@gmail.com>
2019-04-03 16:24:51 +0200

    Forgot to create the table on module update

--- Config.php
+++ Config.php
@@ -17,4 +17,18 @@ class Config extends Module_Config
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;');
 		return true;
 	}
+
+	public function postUpdate_0_2_0(array $data = []): bool
+	{
+		$this->model->_Db->query('CREATE TABLE IF NOT EXISTS `model_seo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `controller` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `keywords` text COLLATE utf8_unicode_ci,
+  `canonical` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;');
+		return true;
+	}
 }
